878e882ea315b34297cbe34a504c75b9bccd6825
leegihe95 <leegihe95@gmail.com>
2022-06-13 18:59:08 +0900

    Built up to Skills page

--- script.js
+++ script.js
@@ -1,9 +1,50 @@
 var animation = bodymovin.loadAnimation({
 	// animationData: { /* ... */ },
-	container: document.getElementById("animationContainer"), // required
+	container: document.getElementById("headerAnimation"), // required
 	path: "./assets/animations/animationHP.json", // required
 	renderer: "svg", // required
 	loop: true, // optional
 	autoplay: true, // optional
 	name: "Demo Animation", // optional
 });
+
+var animation = bodymovin.loadAnimation({
+	// animationData: { /* ... */ },
+	container: document.getElementById("centralAnimation"), // required
+	path: "./assets/animations/welcome.json", // required
+	renderer: "svg", // required
+	loop: true, // optional
+	autoplay: true, // optional
+	name: "Demo Animation", // optional
+});
+
+window.onload = function (){
+	let xhr = new XMLHttpRequest();
+	xhr.open("GET", "./assets/json/skills.json");
+	xhr.addEventListener("load", function (){
+		if(xhr.status == 200){
+			let data = JSON.parse(xhr.response);
+
+			let all_cards = document.getElementById("all_cards");
+			let arr_len = data.skills.length;
+
+			for(let i=0; i<arr_len; i++){
+				let one_card = document.createElement("div");
+				one_card.setAttribute("class", "one_card");
+				
+				let title = document.createElement("h3");
+				title.textContent = data.skills[i].title;
+
+				let image = document.createElement("img");
+				image.src = data.skills[i].image_path;
+
+				// one_card.appendChild(title);
+				one_card.appendChild(image);
+
+				all_cards.appendChild(one_card);
+			}
+
+		}
+	})
+	xhr.send(null);
+}
